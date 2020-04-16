5550aee18d7c56c89a942e94e1befcc176f6e322
ov3rk1ll <overkillerror@gmail.com>
2020-04-16 13:02:17 +0200

    fix missing annotation dependency and demo

--- app/src/main/java/dev/ov3rk1ll/demo/title_card_view/MainActivity.java
+++ app/src/main/java/dev/ov3rk1ll/demo/title_card_view/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import dev.ov3rk1ll.widget.TitleCardView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TitleCardView card = findViewById(R.id.card);
     }
 }
